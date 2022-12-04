01868651c49bc3287c76fcfc3f7ed3b0b23d329b
Sergei Andreyevich <88246593+Implycitt@users.noreply.github.com>
2022-12-03 23:52:05 -0600

    Day 4 input and solution

--- day 4/day4.py
+++ day 4/day4.py
@@ -1,8 +1,66 @@
+import re
 with open("day4.in", 'r') as file:
     data = file.read().split('\n')
 
+def elfAreas(arr):
+    areas = []
+    for i in data:
+        areas.append(re.split(r'[,-]', i))
+    
+    assignmentpairs = 0
+    for i in areas:
+        firstguyarea = []
+        secondguyarea = []
+        for k in range(int(i[0]), int(i[1])+1):
+            firstguyarea.append(k) 
+        for j in range(int(i[2]), int(i[3])+1):
+            secondguyarea.append(j)
+        trueifall = True
+        if len(firstguyarea) < len(secondguyarea):
+            for u in firstguyarea:
+                if u not in secondguyarea:
+                    trueifall = False
+                    break
+            if trueifall:
+                assignmentpairs += 1
+        else:
+            for u in secondguyarea:
+                if u not in firstguyarea:
+                    trueifall = False
+                    break
+            if trueifall:
+                assignmentpairs += 1
+    print(assignmentpairs)
+
+def overlapatall(arr):
+    areas = []
+    for i in data:
+        areas.append(re.split(r'[,-]', i))
+
+    pairs = 0
+    for i in areas:
+        firstguyarea = []
+        secondguyarea = []
+        for k in range(int(i[0]), int(i[1])+1):
+            firstguyarea.append(k) 
+        for j in range(int(i[2]), int(i[3])+1):
+            secondguyarea.append(j)
+
+        if len(firstguyarea) < len(secondguyarea):
+            for u in firstguyarea:
+                if u in secondguyarea:
+                    pairs += 1
+                    break
+        else:
+            for u in secondguyarea:
+                if u in firstguyarea:
+                    pairs += 1
+                    break
+    print(pairs)
+
 def main():
-    return data
+    # elfAreas(data)
+    overlapatall(data)
 
 if __name__ == "__main__":
     print(main())
