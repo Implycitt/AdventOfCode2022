dc999f470d2e77bc81edf3b106f06afba9b84a77
Sergei Andreyevich <88246593+Implycitt@users.noreply.github.com>
2022-12-14 12:42:17 -0600

    Day 11 solution

--- day 11/day11.py
+++ day 11/day11.py
@@ -1,12 +1,54 @@
-with open("day11.in", 'r') as file:
-    data = file.read().split('\n\n')
+monkeys = []
 
-class Monkey:
-    def __init__(self, *startingitems, operation, test, inspects):
-        self.startingitems = startingitems
-        self.operation = operation
-        self.test = test
-        self.inspects = inspects
+for group in open("day11.in", 'r').read().strip().split("\n\n"):
+    data = group.splitlines()
+    monkey = []
+    monkey.append(list(map(int, data[1].split(": ")[1].split(", "))))
+    monkey.append(eval("lambda old:" + data[2].split('=')[1]))
+    for part in data[3:]:
+        monkey.append(int(part.split()[-1]))
+    monkeys.append(monkey)
+
+
+def simulate1():
+    count = [0] * len(monkeys)
+    for i in range(20):
+        for index, monkey, in enumerate(monkeys):
+            for item in monkey[0]:
+                item = monkey[1](item)
+                item //= 3
+                if item % monkey[2] == 0:
+                    monkeys[monkey[3]][0].append(item)
+                else:
+                    monkeys[monkey[4]][0].append(item)
+            count[index] += len(monkey[0])
+            monkey[0] = []
+    return count
+
+def simulate2():
+    count = [0] * len(monkeys)
+    mod = 1
+    for monkey in monkeys:
+            mod *= monkey[2]
+
+    for i in range(10000):
+        for index, monkey in enumerate(monkeys):
+            for item in monkey[0]:
+                item = monkey[1](item)
+                item %= mod
+                if item % monkey[2] == 0:
+                    monkeys[monkey[3]][0].append(item)
+                else:
+                    monkeys[monkey[4]][0].append(item)
+            count[index] += len(monkey[0])
+            monkey[0] = []
+    return count
+
 
 if __name__ == "__main__":
-    print(data)
+    count = simulate1()
+    count.sort()
+    print(count[-1] * count[-2])
+    count = simulate2()
+    count.sort()
+    print(count[-1] * count[-2])
